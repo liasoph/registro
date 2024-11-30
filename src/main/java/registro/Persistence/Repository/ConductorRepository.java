da3b899919f96878538b430d2f66ad43d247330a
liasoph <liannys12@hotmail.com>
2024-11-29 21:08:31 -0500

    actividad

--- src/main/java/registro/Persistence/Repository/ConductorRepository.java
+++ src/main/java/registro/Persistence/Repository/ConductorRepository.java
@@ -1,8 +1,10 @@
 package registro.Persistence.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import registro.Persistence.entity.Conductor;
 
+@Repository
 public interface ConductorRepository extends JpaRepository<Conductor, Long> {
 
 }
--- src/main/java/registro/Persistence/Repository/VehiculoRepository.java
+++ src/main/java/registro/Persistence/Repository/VehiculoRepository.java
@@ -1,8 +1,10 @@
 package registro.Persistence.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import registro.Persistence.entity.Vehiculo;
 
+@Repository
 public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
 
 }
